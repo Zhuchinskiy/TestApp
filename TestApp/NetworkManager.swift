1bd60dbeccb55be81f76672dc95fae7b78088611
Alexandr Zhuchinskiy <ozhu@ciklum.com>
2017-01-04 14:07:09 +0200

    Initial commit.

---
+++ TestApp/NetworkManager.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkManager.swift
+//  TestApp
+//
+//  Created by Alexandr Zhuchinskiy on 1/4/17.
+//  Copyright © 2017 Alexandr Zhuchinskiy. All rights reserved.
+//
+
+import UIKit
+
+class NetworkManager {
+
+    static let baseURL = " /2.0/?method=geo.gettopartists&country=spain&api_key=YOUR_API_KEY&format=json"
+    static let apiKey = "e81f61890b7ff8633ca024d0faa449e7"
+    
+    
+    
+}
